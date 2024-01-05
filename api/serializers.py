2603f2d60dfca227c649e85dfa09f14bf683406f
kiryan <kiryan5480@yandex.ru>
2024-01-05 11:41:46 +0300

    create serializers

---
+++ api/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers, validators
+
+from api.models import ApiUser, Hotel
+
+
+class UserSerialiser(serializers.Serializer):
+    username = serializers.CharField(
+        max_length=128,
+        validators=[
+            validators.UniqueValidator(ApiUser.objects.all())
+        ]
+    )
+    email = serializers.EmailField(
+        validators=[
+            validators.UniqueValidator(ApiUser.objects.all())
+        ]
+    )
+    password = serializers.CharField(min_length=6, max_length=20, write_only=True)
+
+
+    def update(self, instance, validated_data):
+        if email := validated_data.get('email'):
+            instance.emaile = email
+            instance.save(update_fields=['password'])
+
+        if password := validated_data.get('password'):
+            instance.set_passwod(password)
+            instance.save(update_fields=['password'])
+        return instance
+
+    def create(self, validated_data):
+        user = ApiUser.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+        )
+
+        user.set_passwod(validated_data['password'])
+        user.save(update_fields=['password'])
+        return user
+
+
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = '__all__'
+        extra_kwargs = {'id': {'read_only': True}}
